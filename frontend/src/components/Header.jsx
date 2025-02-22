d338fd1609e32662f87d7e951bd56e320ef5386d
ChidiogoVictor <chidiogovictor10@gmail.com>
2025-02-22 22:50:14 +0100

    add images

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Logo from "./Logo";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="h-16">
+      <div className="container mx-auto">
+        <div className="">
+          <Logo w={100} h={50} />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
