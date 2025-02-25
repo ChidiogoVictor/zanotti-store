88bbfbc72e51a31d043e87452fa9ded48100e568
ChidiogoVictor <chidiogovictor10@gmail.com>
2025-02-25 15:10:58 +0100

    add login button and change background color

--- frontend/routes/index.jsx
+++ frontend/routes/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../src/App";
 import Home from "../src/pages/Home";
+import Login from "../src/pages/Login";
 
 const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+      },
+      {
+        path: "login",
+        element: <Login />,
       },
     ],
   },
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,13 +4,16 @@ import Logo from "./Logo";
 import { FiSearch } from "react-icons/fi";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { FaCartPlus } from "react-icons/fa";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className="h-16 shadow-md">
+    <header className="h-16 shadow-md bg-purple-400">
       <div className="h-full container mx-auto flex items-center px-4 justify-between">
         <div>
-          <Logo w={90} h={50} />
+          <Link to={"/"}>
+            <Logo w={90} h={50} />
+          </Link>
         </div>
 
         <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow-md pl-2">
@@ -24,17 +27,26 @@ const Header = () => {
           </div>
         </div>
 
-        <div className="flex items-center gap-4">
+        <div className="flex items-center gap-7">
           <div className="text-2xl">
             <FaRegCircleUser />
-            
           </div>
 
-          <div className="text-xl">
-            <span><FaCartPlus /></span>
-            <div className="bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center">
+          <div className="text-xl relative">
+            <span>
+              <FaCartPlus />
+            </span>
+            <div className="bg-red-600 text-white w-5 h-5 rounded-full p-1 flex items-center justify-center absolute -top-3 -right-3">
               <p className="text-sm">0</p>
             </div>
+          </div>
+
+          <div>
+            <Link to={"/login"}>
+              <button className="px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700">
+                Login
+              </button>
+            </Link>
           </div>
         </div>
       </div>
