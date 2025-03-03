a1efb54703bac7a3612dfb7ee7b66f95673b1b8d
ChidiogoVictor <chidiogovictor10@gmail.com>
2025-03-03 14:48:22 +0100

    complete signup page

--- frontend/src/pages/SIgnUp.jsx
+++ frontend/src/pages/SIgnUp.jsx
@@ -7,9 +7,13 @@ import { Link } from "react-router-dom";
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
   const [data, setData] = useState({
     email: "",
     password: "",
+    name: "",
+    ConfirmPassword: "",
+    profilepic: "",
   });
 
   const handleOnChange = (e) => {
@@ -39,6 +43,20 @@ const SignUp = () => {
 
           <form className="" onSubmit={handleSubmit}>
             <div className="my-2">
+              <label>Name: </label>
+              <div className="bg-slate-100 p-2 rounded">
+                <input
+                  type="text"
+                  placeholder="enter name"
+                  name="name"
+                  value={data.name}
+                  onChange={handleOnChange}
+                  className="w-full h-full outline-none bg-transparent"
+                />
+              </div>
+            </div>
+
+            <div className="my-2">
               <label>Email: </label>
               <div className="bg-slate-100 p-2 rounded">
                 <input
@@ -52,12 +70,12 @@ const SignUp = () => {
               </div>
             </div>
 
-            <div>
+            <div className="my-2">
               <label>Password: </label>
               <div className="bg-slate-100 p-2 rounded flex">
                 <input
                   type={showPassword ? "text" : "password"}
-                  placeholder="password"
+                  placeholder="enter password"
                   name="password"
                   value={data.password}
                   onChange={handleOnChange}
@@ -71,25 +89,41 @@ const SignUp = () => {
                 </div>
               </div>
             </div>
-            <Link
-              to={"/forgot-password"}
-              className="block w-fit ml-auto hover:underline hover:text-red-500"
-            >
-              Forgot password ?
-            </Link>
+
+            <div>
+              <label>Confirm Password: </label>
+              <div className="bg-slate-100 p-2 rounded flex">
+                <input
+                  type={showConfirmPassword ? "text" : "password"}
+                  placeholder="confirm password"
+                  name="confirmPassword"
+                  value={data.ConfirmPassword}
+                  onChange={handleOnChange}
+                  className="w-full h-full outline-none bg-transparent"
+                />
+                <div
+                  className="cursor-pointer text-xl"
+                  onClick={() => setShowConfirmPassword((prev) => !prev)}
+                >
+                  <span>
+                    {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
+                  </span>
+                </div>
+              </div>
+            </div>
 
             <button className="bg-red-600 text-white px-6 py-2 mt-5 w-full max-w-[150px] mx-auto block rounded-full hover:scale-110 hover:bg-red-700 transition-all ">
-              Login
+              Sign Up
             </button>
           </form>
 
           <p className="mt-3">
-            Don't have account ?{" "}
+            Already have account ?{" "}
             <Link
-              to={"/sign-up"}
+              to={"/login"}
               className="text-red-600 hover:text-red-700 hover:underline"
             >
-              Sign up
+              Login
             </Link>
           </p>
         </div>
